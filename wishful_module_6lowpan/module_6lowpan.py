53fdaea5b7b2e1d9e2701cb5a1d00941af59bc58
Matevz Vucnik <vucnikm@gmail.com>
2017-10-12 11:58:42 +0200

    add iperf support for 6lowpan packet flow tests

--- wishful_module_6lowpan/module_6lowpan.py
+++ wishful_module_6lowpan/module_6lowpan.py
@@ -4,6 +4,9 @@ import wishful_upis as upis
 import wishful_framework as wishful_module
 from wishful_framework.classes import exceptions
 
+import iperf3
+from time import sleep
+
 __author__ = "Matevz Vucnik"
 __copyright__ = "Copyright (c) 2017, Jozef Stefan Instiute"
 __version__ = "0.1.0"
@@ -12,12 +15,29 @@ __email__ = "matevz.vucnik@ijs.si"
 @wishful_module.build_module
 class SixlowpanModule(wishful_module.AgentModule):
 
-    def __init__(self, service, serial):
+    def __init__(self):
         super(SixlowpanModule, self).__init__()
         self.log = logging.getLogger('SixlowpanModule')
 
-    @wishful_module.bind_function(upis.radio.get_measurements)
-    def get_measurements(self, params):
-        return {'hello':params[0]}
+    @wishful_module.bind_function(upis.net.create_packetflow_sink)
+    def create_packetflow_sink(self, port):
+        self.log.debug("Starts iperf server on port {}".format(port))
+        server = iperf3.Server()
+        server.port = port
+        server.run()
+        return "Iperf server exited"
 
-    # TODO: Implement real 6LoWPAN UPIs
+    @wishful_module.bind_function(upis.net.start_packetflow)
+    def start_packetflow(self, dest_ip, port):
+        self.log.debug("Start iperf client.")
+        client = iperf3.Client()
+        client.duration = 1
+        client.server_hostname = dest_ip
+        client.port = port
+        for i in range(10):
+            result = client.run()
+            if result.error:
+                sleep(1)
+            else:
+                break
+        return result
